8f41ec22f8cca90f618a54f5d96a9d3cae347fc7
Aastha Patel <114515584+Aastha-P@users.noreply.github.com>
2023-05-10 00:45:45 +0530

    General Size Matrix

---
+++ Pattern_Problem/GeneralSizeMatrix.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, m;
+    cout << "Enter the size of row of Matrix: ";
+    cin >> n;
+    cout << "Enter the size ofcolumn of Matrix: ";
+    cin >> m;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            printf(" * ");
+        }
+        printf("\n");
+    }
+}
