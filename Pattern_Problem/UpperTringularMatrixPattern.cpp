26e45c0c8445978245d1b643696f52fea9f0226c
Aastha Patel <114515584+Aastha-P@users.noreply.github.com>
2023-05-11 00:35:49 +0530

    Upper Matrix

---
+++ Pattern_Problem/UpperTringularMatrixPattern.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the size of Matrix: ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (i < j)
+                printf(" * ");
+            else
+                printf("   ");
+        }
+        printf("\n");
+    }
+}
