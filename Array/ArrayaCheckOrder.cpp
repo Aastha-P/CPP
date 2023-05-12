0371724957ffca37b390f746c42230413d64bd01
Aastha Patel <114515584+Aastha-P@users.noreply.github.com>
2023-05-13 00:19:30 +0530

    Ascending or not
    Here we check array is in ascending order or not.

---
+++ Array/ArrayaCheckOrder.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, i, j, f = 0;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+    int arr[n];
+    for (i = 0; i < n; i++)
+    {
+        cin >> arr[n];
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[i])
+                f = 1;
+            break;
+        }
+    }
+    if (f == 0)
+    {
+        cout << "Arrry in ascending order.";
+    }
+    else
+        cout << "Array is not in ascending order.";
+
+    return 0;
+}
