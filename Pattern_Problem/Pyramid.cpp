1ebeb5aa93a224bba104fb1fcb915f8525c3a546
Aastha Patel <114515584+Aastha-P@users.noreply.github.com>
2023-06-01 00:13:38 +0530

    Pyramid Pattern

--- Pattern_Problem/Pyramid.cpp
+++ Pattern_Problem/Pyramid.cpp
@@ -13,7 +13,7 @@ int main()
             printf(" ");
         }
 
-        for (int k = 0; k < (2 * i) + 1; k++)
+        for (int k = 0; k < (2 * i) -1; k++)
         {
             printf("*");
         }
