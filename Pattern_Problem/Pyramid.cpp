73465ee1e8fde93090f5988c2c6510ad2a0ea4ff
Aastha Patel <114515584+Aastha-P@users.noreply.github.com>
2023-05-31 23:37:18 +0530

    Pyramid Pattern

--- Pattern_Problem/Pyramid.cpp
+++ Pattern_Problem/Pyramid.cpp
@@ -4,11 +4,8 @@ int main()
 {
     int rows = 5;
 
-   
     for (int i = 0; i < rows; i++)
-    {
-
-        
+    {  
         for (int j = 0; j < 2 * (rows - i) - 1; j++)
         {
             printf(" ");
