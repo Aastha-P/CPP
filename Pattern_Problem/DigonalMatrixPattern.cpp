41678a9e2b83fe334a216553d9da0fd0d700725a
Aastha Patel <114515584+Aastha-P@users.noreply.github.com>
2023-06-05 20:40:14 +0530

    Digonal Matrix

--- Pattern_Problem/DigonalMatrixPattern.cpp
+++ Pattern_Problem/DigonalMatrixPattern.cpp
@@ -10,10 +10,11 @@ int main()
         for (int j = 1; j <= n; j++)
         {
             if (i == j)
-                printf(" * ");
+                cout<<" * ";
             else
-                printf(" ");
+                cout<<" ";
         }
-        printf("\n");
+        cout<<endl;
     }
+    return 0;
 }
