2b0b1da6b09822c8c17eff939662bc175a4a481c
Aastha Patel <114515584+Aastha-P@users.noreply.github.com>
2023-06-05 20:42:11 +0530

    General Size Matrix

--- Pattern_Problem/GeneralSizeMatrix.cpp
+++ Pattern_Problem/GeneralSizeMatrix.cpp
@@ -12,8 +12,9 @@ int main()
     {
         for (int j = 1; j <= m; j++)
         {
-            printf(" * ");
+            cout<<" * ";
         }
-        printf("\n");
+        cout<<endl;
     }
+    return 0;
 }
